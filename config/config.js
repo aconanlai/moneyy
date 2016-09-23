2e31075893b7b68fccc0e59502064906cb1cd051
conan <aconanlai@gmail.com>
2016-09-23 18:21:11 -0400

    specify node version

--- config/config.js
+++ config/config.js
@@ -3,5 +3,4 @@ module.exports = {
   node_env: process.env.NODE_ENV || 'development',
   morgan_log_level: process.env.MORGAN_LOG_LEVEL || 'dev',
   mongodb_uri: process.env.MONGODB_URI || 'mongodb://127.0.0.01:27017/money'
-
 };
