9e363afab715d7da69fe6121678ff4160a51eeb1
usrrname <jennifer7chan@gmail.com>
2016-09-23 18:01:50 -0400

    resolve again

--- config/config.js
+++ config/config.js
@@ -2,9 +2,6 @@ module.exports = {
   port: process.env.PORT || 3000,
   node_env: process.env.NODE_ENV || 'development',
   morgan_log_level: process.env.MORGAN_LOG_LEVEL || 'dev',
-  mongodb_uri: process.env.MONGODB_URI || 'mongodb://127.0.0.01:27017/money',
-<<<<<<< 621133872e0d9a77553e17a08cdb114979184129
+  mongodb_uri: process.env.MONGODB_URI || 'mongodb://127.0.0.01:27017/money'
+
 };
-=======
-};
->>>>>>> changed localhost to 127.0.0.01
