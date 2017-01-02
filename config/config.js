f34aac920f30d2714fa6cb37d93122e66350f380
conan <aconanlai@gmail.com>
2017-01-02 01:33:36 -0500

    todo: remove secret, set process var on localhost

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
-const secret = require('./secret');
+// const secret = require('./secret');
 
 module.exports = {
   port: process.env.PORT || 3000,
   node_env: process.env.NODE_ENV || 'development',
   morgan_log_level: process.env.MORGAN_LOG_LEVEL || 'dev',
-  // mongodb_uri: process.env.MONGODB_URI,
-  mongodb_uri: process.env.MONGODB_URI || secret.mongo,
+  mongodb_uri: process.env.MONGODB_URI,
+  // mongodb_uri: process.env.MONGODB_URI || secret.mongo,
 };
